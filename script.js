8b619644437c7409daf1a501f01ad1550e962bed
Will Cote <90800455+willcote@users.noreply.github.com>
2023-04-28 16:44:08 -0400

    Complete change grid resolution button functionality

--- script.js
+++ script.js
@@ -1,28 +1,83 @@
-/* create 16 x 16 grid */
+createGrid(16);
 
-const gridContainer = document.querySelector(".grid-container");
+/* change resolution button */
 
-const GRID_SIZE = 16;
-for (let i = 0; i < GRID_SIZE; i++) {
-  const squareContainer = document.createElement("div");
-  squareContainer.classList.add("square-container");
-  gridContainer.appendChild(squareContainer);
+/* 
+when button is pressed:
+  prompt user for a resolution (fill in whatever current res is)
+  validate input
+  store value and use it change how many squares there are
+*/
 
-  for (let j = 0; j < GRID_SIZE; j++) {
-    const square = document.createElement("div");
-    square.classList.add("square");
+const changeResolutionButton = document.querySelector(
+  ".change-resolution-button"
+);
 
-    squareContainer.appendChild(square);
-  }
+changeResolutionButton.addEventListener("click", changeGridResolution);
+
+function changeGridResolution() {
+  userInput = prompt("Type the desired pixel length");
+  console.log(userInput);
+  while (!validateInput(userInput))
+    userInput = prompt("please choose a whole number from 1-50");
+
+  createGrid(userInput);
 }
 
-/* hover */
+function validateInput(input) {
+  // want it to check if it's
+  // - an integer
+  // - GREATER THAN 0
+  // - LESS THAN <some number, probably like 50>
 
-squares = document.querySelectorAll(".square");
+  if (
+    input == parseInt(input, 10) &&
+    parseInt(input, 10) > 0 &&
+    parseInt(input, 10) <= 50
+  )
+    return true;
+}
+/* create 16 x 16 grid */
 
-// forEach needs squares to be an array instead of a nodelist
-Array.from(squares).forEach((s) => {
-  s.addEventListener("mouseover", () => {
-    s.classList.add("hover");
+function createGrid(numSquares) {
+  const gridContainer = document.querySelector(".grid-container");
+  clearSquares(gridContainer);
+
+  for (let i = 0; i < numSquares; i++) {
+    const squareContainer = document.createElement("div");
+    squareContainer.classList.add("square-container");
+    gridContainer.appendChild(squareContainer);
+
+    for (let j = 0; j < numSquares; j++) {
+      const square = document.createElement("div");
+      square.classList.add("square");
+
+      squareContainer.appendChild(square);
+    }
+  }
+
+  // here so the new squares get the hovering effect
+  // if this was outside the function, only the original squares would get it
+  addHoverFunctionality();
+}
+
+function clearSquares(container) {
+  let gridContents = container.childNodes;
+  Array.from(gridContents).forEach(function (gridRow) {
+    gridRow.remove();
   });
-});
+  console.log(container.childNodes);
+}
+
+function addHoverFunctionality() {
+  /* hover */
+
+  squares = document.querySelectorAll(".square");
+
+  // forEach needs squares to be an array instead of a nodelist
+  Array.from(squares).forEach((s) => {
+    s.addEventListener("mouseover", () => {
+      s.classList.add("hover");
+    });
+  });
+}
